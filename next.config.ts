123ebc2c9b3a541734fcc96b41246b8d4e3f9d13
iloaq <arman.sytch@yandex.ru>
2025-07-07 04:37:05 +0500

    add tailwind

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    optimizeCss: true,
-  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
